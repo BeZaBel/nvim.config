c5c15701e036f1c983b321d07fd5b6cdfaf87496
BeZaBel <azamorabell@gmail.com>
2025-01-25 16:05:28 -0600

    theme

--- lua/setup/plugin_spec.lua
+++ lua/setup/plugin_spec.lua
@@ -218,8 +218,20 @@ return {
                 telescope = true,
                 cmp = true,
             })
-            vim.cmd("colorscheme base16-kanagawa")
-            -- vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+            vim.cmd("colorscheme base16-darktooth")
+
+            -- Set transparent background (should be called after colorscheme setup)
+            -- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "MsgArea", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
+
+            -- Optional: For better transparency consistency
+            -- vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
         end,
     },
     {
